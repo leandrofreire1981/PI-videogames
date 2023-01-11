42b37fec1dad3d984ac50abd5022ca064f648a0e
Leandro Freire <fotografoleandro81@gmail.com>
2023-01-10 21:23:23 -0300

    otro

--- api/index.js
+++ api/index.js
@@ -26,6 +26,6 @@ const port = process.env.PORT || 3001;
 
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at ', port); // eslint-disable-line no-console
   });
 });
